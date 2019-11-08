d9a0b5f6e5c688726c5700c94ef0a4f94813d929
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-11-08 09:45:28 -0700

    added redux and react redux. added files for reducers,actions, smurfcard, and smurf form.

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,11 @@
+
+export const reducer = (state = initialState, action) =>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+const initialState = {
+    smurfList: []
+}
